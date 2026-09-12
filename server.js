11063811e1152f31382673742bb55b690008c106
aniketkumar05123-cpu <aniketkumar051236@gmail.com>
2026-09-12 17:02:49 +0530

    Fix User model import for deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('./models/User');
+const User = require('./models/user');
 const auth = require('./middleware/auth');
 const Review = require('./models/Review');
 
